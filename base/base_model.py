1035d137be4a8db9a88cb472def1a507e448dadd
darkgeek <mohamedshawky911@gmail.com>
2019-06-11 16:14:59 +0200

    Adding base model, trainer and config files

---
+++ base/base_model.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+
+class BaseModel:
+    def __init__(self, config):
+        self.config = config
+        self.init_global_step()
+        self.init_cur_epoch()
+
+    def save(self, sess):
+        print("Saving Model...")
+        self.saver.save(sess, self.config.checkpoint_dir, self.global_step_tensor)
+        print("Model Saved!")
+
+    def load(self, sess):
+        latest_checkpoint = tf.train.latest_checkpoint(self.config.checkpoint_dir)
+        if latest_checkpoint:
+            print("Loading Model Checkpoint {} ....\n".format(latest_checkpoint))
+            self.saver.restore(sess, latest_checkpoint)
+            print("Model loaded!")
+
+    def init_cur_epoch(self):
+        with tf.variable_scope('cur_epoch'):
+            self.cur_epoch_tensor = tf.Variable(0, trainable=False, name='cur_epoch')
+            self.increment_cur_epoch_tensor = tf.assign(self.cur_epoch_tensor, self.cur_epoch_tensor + 1)
+
+    def init_global_step(self):
+        with tf.variable_scope('global_step'):
+            self.global_step_tensor = tf.Variable(0, trainable=False, name='global_step')
+
+    def init_saver(self):
+        raise NotImplementedError   
+
+    def build_model(self):
+        raise NotImplementedError                               
+
+        
+
---
+++ base/base_trainer.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+class BaseTrain:
+    def __init__(self, sess, model, data, config, logger):
+        self.model = model
+        self.logger = logger
+        self.config = config
+        self.sess = sess
+        self.data = data
+        self.init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+        self.sess.run(self.init)
+
+    def train(self):
+        for cur_epoch in range(self.model.cur_epoch_tensor.eval(self.sess), self.config.num_epochs + 1, 1):
+            self.train_epoch()
+            self.sess.run(self.model.increment_cur_epoch_tensor)
+
+    def train_epoch(self):
+        raise NotImplementedError
+
+    def train_step(self):
+        raise NotImplementedError            
