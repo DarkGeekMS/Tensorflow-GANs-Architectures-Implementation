e0b159d0e65c7abbbf738d26efa7607fa5d5fc90
darkgeek <mohamedshawky911@gmail.com>
2019-06-17 15:51:40 +0200

    Finalizing CycleGAN model

--- models/cyclegan_model.py
+++ models/cyclegan_model.py
@@ -111,3 +111,78 @@ class Discriminator:
 class CycleGANModel(BaseModel):
     def __init__(self, config):
         super(CycleGANModel, self).__init__(config)
+        self.batch_size = self.config.batch_size
+        self.image_size = self.config.image_size
+        self.input_c_dim = self.config.input_nc
+        self.output_c_dim = self.config.output_nc
+        self.l1_lambda = self.config.l1_lambda
+        self.data_dir = self.config.data_dir
+        self.dis_a = Discriminator(self.config.df_dim, name="discriminatorA")
+        self.dis_b = Discriminator(self.config.df_dim, name="discriminatorB")
+        self.gen_ab = Generator(self.config.gf_dim, self.output_c_dim, name="generatorA2B")
+        self.gen_ba = Generator(self.config.gf_dim, self.output_c_dim, name="generatorB2A")
+        self.loss = self.mae_criterion
+        self.build_model()
+        self.init_saver()
+
+    def build_model(self):
+        self.real_data = tf.placeholder(tf.float32, 
+                                        [None, self.image_size, self.image_size, 
+                                        self.input_c_dim + self.output_c_dim],
+                                        name="real_images")
+        self.real_A = self.real_data[:,:,:,:self.input_c_dim]
+        self.real_B = self.real_data[:,:,:,self.input_c_dim:self.input_c_dim+self.output_c_dim]
+        
+        self.fake_B = self.gen_ab(self.real_A, reuse=False)
+        self.fake_Acc = self.gen_ba(self.fake_B, reuse=False)
+        self.fake_A = self.gen_ba(self.real_B, reuse=True)
+        self.fake_Bcc = self.gen_ab(self.fake_A, reuse=True)
+
+        self.DA_fake = self.dis_a(self.fake_A, reuse=False)
+        self.DB_fake = self.dis_b(self.fake_B, reuse=False)
+
+        self.g_loss_a2b = self.loss(self.DB_fake, tf.ones_like(self.DB_fake)) \
+                        + self.l1_lambda * self.abs_criterion(self.real_A, self.fake_Acc) \
+                        + self.l1_lambda * self.abs_criterion(self.real_B, self.fake_Bcc) 
+        self.g_loss_b2a = self.loss(self.DA_fake, tf.ones_like(self.DA_fake)) \ 
+                        + self.l1_lambda * self.abs_criterion(self.real_A, self.fake_Acc) \
+                        + self.l1_lambda * self.abs_criterion(self.real_B, self.fake_Bcc)
+        self.g_loss = self.loss(self.DA_fake, tf.ones_like(self.DA_fake)) \ 
+                    + self.loss(self.DB_fake, tf.ones_like(self.DB_fake)) \
+                    + self.l1_lambda * self.abs_criterion(self.real_A, self.fake_Acc) \
+                    + self.l1_lambda * self.abs_criterion(self.real_B, self.fake_Bcc)
+                    
+        self.fake_A_sample = tf.placeholder(tf.float32, 
+                            [None, self.image_size, self.image_size, self.input_c_dim], 
+                            name="fake_A_sample")
+        self.fake_B_sample = tf.placeholder(tf.float32,
+                            [None, self.image_size, self.image_size, self.output_c_dim],
+                            name="fake_B_sample")
+        self.DB_real = self.dis_b(self.real_B, reuse=True)        
+        self.DA_real = self.dis_a(self.real_A, reuse=True)
+        self.DB_fake_sample = self.dis_b(self.fake_B_sample, reuse=True)
+        self.DA_fake_sample = self.dis_a(self.fake_A_sample, reuse=True)
+
+        self.db_loss_real = self.loss(self.DB_real, tf.ones_like(self.DB_real))
+        self.db_loss_fake = self.loss(self.DB_fake_sample, tf.zeros_like(self.DB_fake_sample))
+        self.db_loss = (self.db_loss_real + self.db_loss_fake) / 2
+        self.da_loss_real = self.loss(self.DA_real, tf.ones_like(self.DA_real))
+        self.da_loss_fake = self.loss(self.DA_fake_sample, tf.zeros_like(self.DA_fake_sample))
+        self.da_loss = (self.da_loss_real + self.da_loss_fake) / 2
+        self.d_loss = self.da_loss + self.db_loss
+
+        t_vars = tf.trainable_variables()
+        self.d_vars = [var for var in t_vars if 'discriminator' in var.name]
+        self.g_vars = [var for var in t_vars if 'generator' in var.name]                                                                 
+
+    def init_saver(self):
+        self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
+
+    def mae_criterion(self, inputs, target):
+        return tf.reduce_mean((inputs-target)**2)
+
+    def abs_criterion(self, inputs, target):
+        return tf.reduce_mean(tf.abs(inputs-target))    
+
+    def sample_images(self):
+        raise NotImplementedError            
